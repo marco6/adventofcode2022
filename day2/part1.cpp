bd32ca84ce892d3d32fb8b6aad0201d71be84734
Marco Manino <marco.manino@canonical.com>
2022-12-06 09:55:30 +0100

    Day2

---
+++ day2/part1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+constexpr unsigned char scores[][3] {
+    { 3, 6, 0 },
+    { 0, 3, 6 },
+    { 6, 0, 3 }    
+};
+
+long long int score(int op, int me) {
+    return scores[op][me];
+}
+
+long long int score(int me) {
+    return me + 1;
+}
+
+
+int main() {
+    char op, me;
+    long long int points = 0;
+    while (std::cin >> op && std::cin >> me) {
+        int op_move = op - 'A';
+        int me_move = me - 'X';
+        points += score(me_move) + score(op_move, me_move);
+    }
+
+    std::cout << points << std::endl;
+}
