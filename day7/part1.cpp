3d5bd2aa76405665f186c9a43e74e0877a5f5310
Marco Manino <marco.manino@canonical.com>
2022-12-07 11:31:12 +0100

    Day7

---
+++ day7/part1.cpp
@@ -0,0 +1,83 @@
+#include <cstddef>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <memory>
+#include <utility>
+
+struct dir;
+
+struct dir {
+    size_t size{0};
+    std::unordered_map<std::string, std::unique_ptr<dir>> dirs;
+    dir *parent{nullptr};
+
+    auto& subdir(const std::string& f) {
+        auto& res = dirs[f];
+        if (!res) {
+            res.reset(new dir{});
+            res->parent = this;
+        }
+        return *res;
+    }
+
+    void add_file(size_t s) {
+        size += s;
+        if (this != this->parent) {
+            this->parent->add_file(s);
+        }
+    }
+};
+
+dir* cd(dir& root, dir& wd, const std::string& folder) {
+    if (folder == "..") {
+        return wd.parent;
+    } else if (folder == ".") {
+        return &wd;
+    } else if (folder == "/") {
+        return &root;
+    } else {
+        return &wd.subdir(folder);
+    }
+}
+
+template <typename F>
+void visit(dir& d, F&& f) {
+    f(d);
+    for (auto& dir : d.dirs) {
+        visit(*dir.second, std::forward<F&&>(f));
+    }
+}
+
+int main() {
+    dir root;
+    root.parent = &root;
+
+    dir* wd = &root;
+
+    std::string command;
+
+    while (std::cin >> command) {
+        if (command == "$") {
+            std::cin >> command;
+            if (command == "cd") {
+                std::cin >> command;
+                wd = cd(root, *wd, command);
+            }
+        } else if (command == "dir") {
+            std::cin >> command;
+            wd->subdir(command);
+        } else {
+            auto size = stoul(command);
+            std::cin >> command;
+            wd->add_file(size);
+        }
+    }
+
+    // Now visit them all
+    size_t count = 0;
+    visit(root, [&](dir& d) { if (d.size <= 100000) { count  += d.size; } });
+    
+    std::cout << count << std::endl;
+}
